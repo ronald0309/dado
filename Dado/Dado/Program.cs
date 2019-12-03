ec0c96c3fe10c15f01975c90e5789cf1edf4c512
ronald0309 <ronaldba16@gmail.com>
2019-12-02 22:05:11 -0600

    Se genera el dado

--- Dado/Dado/Program.cs
+++ Dado/Dado/Program.cs
@@ -4,35 +4,35 @@ namespace Dado
 {
     class Program
     {
-        void imprimirDado(int superior, int caraFrontal, int laterales)
+        static void imprimirDado(int superior, int caraFrontal, int laterales)
         {
-            int max = 7;
+            
             imprimirSuperior(superior);
             imprimirCaraFrontal(caraFrontal,laterales);
             imprimirInferior(7 - superior);
 
         }
-        void imprimirSuperior(int superior)
+        static void imprimirSuperior(int superior)
         {
             Console.WriteLine("             ########");
             Console.WriteLine("             #      #");
-            Console.WriteLine("             #   {0}   #",superior);
+            Console.WriteLine("             #   {0}  #",superior);
             Console.WriteLine("             #      #");
 
         }
-        void imprimirCaraFrontal(int caraFrontal, int laterales)
+        static void imprimirCaraFrontal(int caraFrontal, int laterales)
         {
             int max = 7;
             Console.WriteLine("############################");
             Console.WriteLine("#     #      #      #      #");
-            Console.WriteLine("#  {0}   #  {1}    #  {2}    #   {3}   #",7-caraFrontal,7-laterales,caraFrontal,laterales);
+            Console.WriteLine("#  {0}  #  {1}   #  {2}   #   {3}  #",7-caraFrontal,7-laterales,caraFrontal,laterales);
             Console.WriteLine("#     #      #      #      #");
             Console.WriteLine("#############################");
         }
-        void imprimirInferior(int inferior)
+        static void imprimirInferior(int inferior)
         {
             Console.WriteLine("             #      #");
-            Console.WriteLine("             #  {0}    #",inferior);
+            Console.WriteLine("             #  {0}   #",inferior);
             Console.WriteLine("             #      #");
             Console.WriteLine("             ########");
         }
@@ -42,30 +42,23 @@ namespace Dado
             int x;
             int x2;
             int x3;
-            int x4;
-            int X5;
-            int x6;
-
-            do
+            while (Console.ReadKey().Key != ConsoleKey.Escape)
             {
-               
                 if (Console.ReadKey().Key == ConsoleKey.Enter)
                 {
                     x = random.Next(1, 4);
                     do
                     {
                         x2 = random.Next(1, 4);
-                    } while (x == x2);
+                    } while (x == x2|| x == (7-x2));
                     do
                     {
                         x3 = random.Next(1, 4);
-                    } while (x == x3 || x2==x3);
-                    Console.WriteLine("Nueva Visualizacion {0},{1},{2}", x,x2,x3);
-                   // imprimirDado(x,x2,x3);
+                    } while (x == x3 || x2 == x3);
+                    Console.WriteLine("Nueva Visualizacion {0},{1},{2}", x, x2, x3);
+                    imprimirDado(x, x2, x3);
                 }
-            } while (Console.ReadKey().Key == ConsoleKey.Escape);
-
-
+            }
         }
     }
 }
