1ce26331da5b18526552d54dc121c3caa93707f8
ronald0309 <ronaldba16@gmail.com>
2019-12-02 22:28:16 -0600

    valedaciones de teclado

--- Dado/Dado/Program.cs
+++ Dado/Dado/Program.cs
@@ -4,6 +4,7 @@ namespace Dado
 {
     class Program
     {
+        // funcion principal para imprimir el dado 
         static void imprimirDado(int superior, int caraFrontal, int laterales)
         {
             
@@ -12,6 +13,7 @@ namespace Dado
             imprimirInferior(7 - superior);
 
         }
+        //imprime la cara superior del dado 
         static void imprimirSuperior(int superior)
         {
             Console.WriteLine("             ########");
@@ -20,6 +22,7 @@ namespace Dado
             Console.WriteLine("             #      #");
 
         }
+        //imprime las caras, laterales, fontal y tracera, del dado 
         static void imprimirCaraFrontal(int caraFrontal, int laterales)
         {
             int max = 7;
@@ -29,6 +32,7 @@ namespace Dado
             Console.WriteLine("#     #      #      #      #");
             Console.WriteLine("#############################");
         }
+        // imprime la parte inferior del dado 
         static void imprimirInferior(int inferior)
         {
             Console.WriteLine("             #      #");
@@ -42,21 +46,24 @@ namespace Dado
             int x;
             int x2;
             int x3;
+
             while (Console.ReadKey().Key != ConsoleKey.Escape)
             {
-                if (Console.ReadKey().Key == ConsoleKey.Enter)
+                while (Console.ReadKey().Key != ConsoleKey.Escape && Console.ReadKey().Key == ConsoleKey.Enter)
                 {
+
                     x = random.Next(1, 4);
                     do
                     {
                         x2 = random.Next(1, 4);
-                    } while (x == x2|| x == (7-x2));
+                    } while (x == x2 || x == (7 - x2));
                     do
                     {
                         x3 = random.Next(1, 4);
                     } while (x == x3 || x2 == x3);
                     Console.WriteLine("Nueva Visualizacion {0},{1},{2}", x, x2, x3);
                     imprimirDado(x, x2, x3);
+
                 }
             }
         }
